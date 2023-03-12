913464d1f235a8d560bd5cdc22cf4a06299578ad
Nusrat <nusrattanha63418@gmail.com>
2023-03-13 02:00:23 +0600

    complete project

---
+++ nosto-bank-js/deposit-practice.js
@@ -0,0 +1,34 @@
+// DRY ---> Do Not Repeat Yourself
+document.getElementById("btn-deposit").addEventListener("click", function () {
+  /* 
+    1. get the element by id
+    2. get the value from the element
+    3. convert string value to a number
+    */
+  const newDepositAmount = getInputFieldValueById("deposit-field");
+  if(isNaN(newDepositAmount))
+  {
+    alert('Please Provide a number');
+    return;
+  }
+  else if(newDepositAmount <0){
+    alert('Please provide a positive number');
+    return;
+  }
+  /* 
+    1. get previous deposit total by id
+
+    */
+  const previousDepositTotal = getTextElementValueById("deposit-total");
+
+  // calculate new deposit total
+  const newDepositTotal = previousDepositTotal + newDepositAmount;
+  
+  // set deposit total value
+  setTextElementValueById("deposit-total", newDepositTotal);
+
+  // get previous balance by using the function
+  const previousBalanceTotal = getTextElementValueById("balance-total");
+  const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+  setTextElementValueById("balance-total", newBalanceTotal);
+});
